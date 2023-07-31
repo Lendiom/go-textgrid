023b1f50e02a809f7b1e9ddde5b3651ce0025a1b
Bradley Hilton <graywolf336@craftyn.com>
2023-07-31 08:25:06 -0500

    Add more campaign information; including dca sharing status (#2)

--- campaign.go
+++ campaign.go
@@ -31,46 +31,69 @@ type CreateCampaignPayload struct {
 }
 
 type Campaign struct {
-	ID                 string    `json:"campaignId"`
-	BrandID            string    `json:"brandId"`
-	CspID              string    `json:"cspId"`
-	ResellerID         string    `json:"resellerId"`
-	ReferenceID        string    `json:"referenceId"`
-	Status             string    `json:"status"`
-	CreateDate         string    `json:"createDate"`
-	BilledDate         string    `json:"billedDate"`
-	AutoRenewal        bool      `json:"autoRenewal"`
-	Usecase            UseCase   `json:"usecase"`
-	SubUsecases        []UseCase `json:"subUsecases"`
-	Description        string    `json:"description"`
-	EmbeddedLink       bool      `json:"embeddedLink"`
-	EmbeddedPhone      bool      `json:"embeddedPhone"`
-	AffiliateMarketing bool      `json:"affiliateMarketing"`
-	NumberPool         bool      `json:"numberPool"`
-	AgeGated           bool      `json:"ageGated"`
-	DirectLending      bool      `json:"directLending"`
-	SubscriberOptin    bool      `json:"subscriberOptin"`
-	SubscriberOptout   bool      `json:"subscriberOptout"`
-	SubscriberHelp     bool      `json:"subscriberHelp"`
-	Sample1            string    `json:"sample1"`
-	Sample2            string    `json:"sample2"`
-	Sample3            string    `json:"sample3"`
-	Sample4            string    `json:"sample4"`
-	Sample5            string    `json:"sample5"`
-	MessageFlow        string    `json:"messageFlow"`
-	HelpKeywords       string    `json:"helpKeywords"`
-	HelpMessage        string    `json:"helpMessage"`
-	OptinKeywords      string    `json:"optinKeywords"`
-	OptinMessage       string    `json:"optinMessage"`
-	OptoutKeywords     string    `json:"optoutKeywords"`
-	OptoutMessage      string    `json:"optoutMessage"`
+	ID                 string                    `json:"campaignId"`
+	AccountSid         string                    `json:"accountSid"`
+	BrandID            string                    `json:"brandId"`
+	CspID              string                    `json:"cspId"`
+	ResellerID         string                    `json:"resellerId"`
+	ReferenceID        string                    `json:"referenceId"`
+	Status             string                    `json:"status"`
+	DateCreated        TextGridTime              `json:"dateCreated"`
+	BilledDate         TextGridTime              `json:"billedDate"`
+	AutoRenewal        bool                      `json:"autoRenewal"`
+	Usecase            UseCase                   `json:"usecase"`
+	SubUsecases        []UseCase                 `json:"subUsecases"`
+	Description        string                    `json:"description"`
+	EmbeddedLink       bool                      `json:"embeddedLink"`
+	EmbeddedPhone      bool                      `json:"embeddedPhone"`
+	AffiliateMarketing bool                      `json:"affiliateMarketing"`
+	NumberPool         bool                      `json:"numberPool"`
+	AgeGated           bool                      `json:"ageGated"`
+	DirectLending      bool                      `json:"directLending"`
+	SubscriberOptin    bool                      `json:"subscriberOptin"`
+	SubscriberOptout   bool                      `json:"subscriberOptout"`
+	SubscriberHelp     bool                      `json:"subscriberHelp"`
+	Sample1            string                    `json:"sample1,omitempty"`
+	Sample2            string                    `json:"sample2,omitempty"`
+	Sample3            string                    `json:"sample3,omitempty"`
+	Sample4            string                    `json:"sample4,omitempty"`
+	Sample5            string                    `json:"sample5,omitempty"`
+	MessageFlow        string                    `json:"messageFlow"`
+	HelpKeywords       string                    `json:"helpKeywords"`
+	HelpMessage        string                    `json:"helpMessage"`
+	OptinKeywords      string                    `json:"optinKeywords"`
+	OptinMessage       string                    `json:"optinMessage"`
+	OptoutKeywords     string                    `json:"optoutKeywords"`
+	OptoutMessage      string                    `json:"optoutMessage"`
+	DcaSharingStatus   SecondaryDcaSharingStatus `json:"SecondaryDcaSharingStatus"`
+	DcaDeclineReason   string                    `json:"SecondaryDcaDeclineReason"`
 }
 
-// CreateBrand submits a brand for registration
+type SecondaryDcaSharingStatus string
+
+var (
+	SecondaryDcaSharingStatusBlank    SecondaryDcaSharingStatus = ""
+	SecondaryDcaSharingStatusPending  SecondaryDcaSharingStatus = "PENDING"
+	SecondaryDcaSharingStatusAccepted SecondaryDcaSharingStatus = "ACCEPTED"
+	SecondaryDcaSharingStatusDeclined SecondaryDcaSharingStatus = "DECLINED"
+)
+
+// CreateCampaign submits a campaign for registration and review
 func (t *textGrid) CreateCampaign(payload CreateCampaignPayload) (*Campaign, error) {
 	result := new(Campaign)
 
 	if err := t.post("campaigns/campaign", payload, result); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// GetCampaign gets the campaign details by the provided id
+func (t *textGrid) GetCampaign(id string) (*Campaign, error) {
+	result := new(Campaign)
+
+	if err := t.get("campaigns/campaign/"+id, nil, result); err != nil {
 		return nil, err
 	}
 
